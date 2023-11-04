38089db9a85af97df89e80a06a419c6486945777
Rachel Struthers <rachel.struthers@sap.com>
2023-11-04 17:09:58 -0500

    Basic node server plus react

---
+++ client/src/App.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+
+async function getUsers() {
+    const baseURL = process.env.REACT_APP_API_URL || "";
+    
+    console.log(`*** baseURL: ${baseURL}`)
+
+    const response = await fetch(`${baseURL}/users`);
+
+    if (!response.ok) {
+        throw new Error(await response.text());
+    }
+
+    const users = await response.json();
+
+    return users;
+}
+
+export function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            setUsers(await getUsers());
+        })();
+    }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <h1>My App</h1>
+            <div>
+                <h2>Users</h2>
+                <p>Users are fetched from the Node.js server that serves this app</p>
+                <p>
+                    In the server the users is a .json file however, you would probably
+                    swap this out for database connection in production!
+                </p>
+                {users.map((user) => (
+                    <div key={user.name}>
+                        <h3>{user.name}</h3>
+                        <a target="_blank" style={{color: "white"}} href={user.website}>{user.website}</a>
+                    </div>
+                ))}
+            </div>
+            </header>
+        </div>
+    );
+}
---
+++ server/src/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const expressStaticGzip = require("express-static-gzip");
+const path = require("path");
+
+const STATIC_FOLDER = path.join(__dirname, "../", "../", "client/", "build/");
+const HTTP_PORT = Number(process.env.HTTP_PORT || 5000);
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.get("/users", (_, res) => res.json(require("./users.json")));
+app.use(expressStaticGzip(STATIC_FOLDER));
+app.get("*", expressStaticGzip(STATIC_FOLDER));
+app.use("*", expressStaticGzip(STATIC_FOLDER));
+
+async function listen() {
+  await app.listen(HTTP_PORT);
+
+  console.log(`Serving files in ${STATIC_FOLDER} on port ${HTTP_PORT}`);
+}
+
+module.exports = {
+  app,
+  listen,
+};
