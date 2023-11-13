922c7fff15074f9b60eee38a957f75b04e06ef01
Rachel Struthers <rstruthers1@yahoo.com>
2023-11-13 09:24:58 -0600

    DORN-9: Try ChatGPT style for menu

--- client/src/routes/root.jsx
+++ client/src/routes/root.jsx
@@ -1,16 +1,15 @@
-import {Link, Outlet} from "react-router-dom";
+import {NavLink, Outlet} from "react-router-dom";
+import './Menu.css';
 
 export default function Root() {
     return (
         <div>
-            <nav style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <div>
-                    <Link to="/">Home</Link> | {' '}
-                    <Link to="/about">About</Link>
-                </div>
-                <div>
-                    <Link to="/signup">Signup</Link> | {' '}
-                    <Link to="/login">Login</Link>
+        <nav className="topMenu">
+            <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
+            <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : ''}>About</NavLink>
+            <div className="loginLink">
+                <NavLink to="/signup" className={({ isActive }) => isActive ? 'active' : ''}>Sign Up</NavLink>
+                <NavLink to="/login" className={({ isActive }) => isActive ? 'active' : ''}>Login</NavLink>
                 </div>
             </nav>
             <Outlet/>
